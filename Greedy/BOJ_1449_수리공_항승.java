26d2f1796f944e411d53a60c3ab397d1a037a756
radio959 <76995452+radio959@users.noreply.github.com>
2021-07-03 16:37:26 +0900

    Create BOJ_1449_수리공_항승.java

---
+++ Greedy/BOJ_1449_수리공_항승.java
@@ -0,0 +1,35 @@
+package Greedy_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1449_수리공_항승 {
+	static int N, L;
+	static int arr[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		
+		int cnt =0;
+		int guard =0;
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i=0; i<N; i++) {
+			if(arr[i]<=guard) continue;
+			guard = arr[i]+(L-1);
+			cnt++;
+		}
+		System.out.println(cnt);
+	}
+
+}
